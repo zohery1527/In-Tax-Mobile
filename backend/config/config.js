a77d658d17093142cdf994d18ae60cddf42f2c96
herizo1527 <herizorakotomamonjy5@gmail.com>
2025-11-07 15:24:57 +0300

    revenir forme initiale

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL || "postgresql://in_tax_db_ijil_user:29Jx5YgqiEXRhK3J30rL8AWNIk0Mz7cc@dpg-d45fpo3uibrs73f4kr40-a.singapore-postgres.render.com/in_tax_db_ijil?ssl=true",
+    username: process.env.DB_USER || "in_tax_db_ijil_user",
+    password: process.env.DB_PASS || "29Jx5YgqiEXRhK3J30rL8AWNIk0Mz7cc",
+    database: process.env.DB_NAME || "in_tax_db_ijil",
+    host: process.env.DB_HOST || "dpg-d45fpo3uibrs73f4kr40-a.singapore-postgres.render.com",
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -13,7 +17,11 @@ module.exports = {
   },
   test: {},
   production: {
-    url: process.env.DATABASE_URL || "postgresql://in_tax_db_ijil_user:29Jx5YgqiEXRhK3J30rL8AWNIk0Mz7cc@dpg-d45fpo3uibrs73f4kr40-a.singapore-postgres.render.com/in_tax_db_ijil?ssl=true",
+    username: process.env.DB_USER || "in_tax_db_ijil_user",
+    password: process.env.DB_PASS || "29Jx5YgqiEXRhK3J30rL8AWNIk0Mz7cc",
+    database: process.env.DB_NAME || "in_tax_db_ijil",
+    host: process.env.DB_HOST || "dpg-d45fpo3uibrs73f4kr40-a.singapore-postgres.render.com",
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
