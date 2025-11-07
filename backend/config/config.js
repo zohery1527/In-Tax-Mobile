a68b1d0d5f2a9862759841d5cb5f909d93d4e6e9
herizo1527 <herizorakotomamonjy5@gmail.com>
2025-11-07 14:22:20 +0300

    suppresion de variable config

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,11 +3,6 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    username: process.env.DB_USER || "in_tax_db_ijil_user",
-    password: process.env.DB_PASS || "29Jx5YgqiEXRhK3J30rL8AWNIk0Mz7cc",
-    database: process.env.DB_NAME || "in_tax_db_ijil",
-    host: process.env.DB_HOST || "dpg-d45fpo3uibrs73f4kr40-a.singapore-postgres.render.com",
-    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -19,11 +14,6 @@ module.exports = {
   test: {},
   production: {
     url: process.env.DATABASE_URL,
-    username: process.env.DB_USER || "in_tax_db_ijil_user",
-    password: process.env.DB_PASS || "29Jx5YgqiEXRhK3J30rL8AWNIk0Mz7cc",
-    database: process.env.DB_NAME || "in_tax_db_ijil",
-    host: process.env.DB_HOST || "dpg-d45fpo3uibrs73f4kr40-a.singapore-postgres.render.com",
-    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
